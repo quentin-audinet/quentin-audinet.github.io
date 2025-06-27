d9cc7a0bd07e95e5cadbd6dcac32d2a5f935d134
Quentin Audinet <86238615+quentin-audinet@users.noreply.github.com>
2025-06-27 20:58:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert("Hello!");
+fetch("https://ooooo.free.beeceptor.com", {
+  method: "POST",
+  body: JSON.stringify({
+    username:"test",
+		secret:"secret"
+  })
+});
+fetch("http://ooooo.free.beeceptor.com", {
+  method: "POST",
+  body: JSON.stringify({
+    username:"test",
+		secret:"secret"
+  })
+});
